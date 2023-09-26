3ef81b64625a2e8d73f1d2d33b3bc916009c8659
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-25 21:17:59 -0700

    commented old code

--- public/js/login.js
+++ public/js/login.js
@@ -1,46 +1,110 @@
 // code to handle login form submission here
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // non working code below
 
 
 
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const User = require('./models/User'); // Replace with the path to your existing User model
+// const express = require('express');
+// const router = express.Router();
+// const passport = require('passport');
+// const User = require('./models/User'); // Replace with the path to your existing User model
 
-// Login route
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/dashboard',
-  failureRedirect: '/login',
-}));
+// // Login route
+// router.post('/login', passport.authenticate('local', {
+//   successRedirect: '/dashboard',
+//   failureRedirect: '/login',
+// }));
 
 
 
-// the signup form is on the login page
-// Signup route
-router.post('/login', async (req, res) => {
-  const { username, password, firstname, lastname, email, newsletter } = req.body;
+// // the signup form is on the login page
+// // Signup route
+// router.post('/login', async (req, res) => {
+//   const { username, password, firstname, lastname, email, newsletter } = req.body;
   
-  try {
-    const hashedPassword = await User.hashPassword(password); // Assuming you have a method in your User model to hash passwords
-    const user = new User({
-      username,
-      password: hashedPassword,
-      firstname,
-      lastname,
-      email,
-      newsletter: !!newsletter, // Convert the checkbox value to boolean
-    });
+//   try {
+//     const hashedPassword = await User.hashPassword(password); // Assuming you have a method in your User model to hash passwords
+//     const user = new User({
+//       username,
+//       password: hashedPassword,
+//       firstname,
+//       lastname,
+//       email,
+//       newsletter: !!newsletter, // Convert the checkbox value to boolean
+//     });
 
-    await user.save();
-    res.redirect('/login'); // Redirect to the login page after successful signup
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error'); // Handle any error that occurs during signup
-  }
-});
+//     await user.save();
+//     res.redirect('/login'); // Redirect to the login page after successful signup
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal Server Error'); // Handle any error that occurs during signup
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
 
   
