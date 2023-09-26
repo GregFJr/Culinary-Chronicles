d0feef9b453c442555465b36a76263c2e9825443
unknown <matt.j.velazquez@gmail.com>
2023-09-25 20:26:21 -0700

    updated login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,9 +8,14 @@ router.post('/login', passport.authenticate('local', {
     failureFlash: true
 }));
 
-router.get('/login', (req, res) => {
+router.get('/login', async (req, res) => {
+    try {
     res.render('login');
-});
+    } catch (err) {
+    res.status(500).json(err);
+    }
+    });
+    
 router.get('/logout', async (req, res) => {
     req.logout();
     res.redirect('/');
