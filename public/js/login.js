227c88a3aaa9e9d897cc3fcc0bfa281ea10fe7bc
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-25 21:20:33 -0700

    fixed login js file from pr

--- public/js/login.js
+++ public/js/login.js
@@ -1,67 +1,41 @@
 // code to handle login form submission here
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.json());
 
+const user =[];
 
+app.get('/login', (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+app.post('/login', (req, res) => {
+    const { firstName, lastName, email, password, newsLetter } = req.body;
+    const userExists = users.some(user => user.email === email);
+        userExists = users.some(user => user.password == password);
+        
 
+    if (userExists) {
+        return res.status(400).send('User already exists');
+    }
+    const newUser = {
+        firstName,
+        lastName,
+        email,
+        password,
+        newsLetter: !!newsLetter,
+    };
 
+    users.push(newUser);
 
+    res.redirect('/');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 // non working code below
