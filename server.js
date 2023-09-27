6d00397f4d07b2ca2648e0662a0e1635aa2ed128
GregFJr <finney101690@gmail.com>
2023-09-26 17:22:36 -0700

    optimized login for mobile

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.engine('handlebars', hbs.engine);
