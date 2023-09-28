58215b971562b04360d999031d4a7cbb2fa6760b
GregFJr <finney101690@gmail.com>
2023-09-28 12:22:18 -0700

    deleted commented out redundancy

--- models/User.js
+++ models/User.js
@@ -3,11 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class User extends Model {
-    // checkPassword(loginPW) {
-    //     return bcrypt.compareSync(loginPW, this.password);
-    // }
-}
+class User extends Model {}
 
 User.init(
     {
